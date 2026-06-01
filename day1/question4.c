038ebd6bdbbdff6501dbcbf4712c856f8b9b6d33
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-01 21:29:13 +0530

    q4 is uploaed

---
+++ day1/question4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+printf("enter number = ");
+
+int number, count=0;
+
+scanf("%d", &number);
+
+int numberf =number;
+
+while(number != 0){
+count++;
+}
+
+numberf = number/10;
+
+number=numberf;
+
+printf("digit in number are %d", count);
+
+return 0;
+
+}

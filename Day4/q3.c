6f02cd7bd9268fdff4bf2f3b188c208e7133045a
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-04 21:02:44 +0530

    armstrong number

--- Day4/q3.c
+++ Day4/q3.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-
-int main() {
-    int n, temp, rem, sum = 0;
-
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    temp = n;
-
-    while(temp > 0) {
-        rem = temp % 10;
-        sum = sum + (rem * rem * rem);
-        temp = temp / 10;
-    }
-
-    if(sum == n)
-        printf("Armstrong Number");
-    else
-        printf("Not Armstrong Number");
-
+nclude<math.h>
+#include<stdio.h>
+int main (){
+    int n,n1,n2,r,d=0,armstrong=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    n1=n2=n;
+    while(n1>0)
+    {
+     d++;
+     n1 =n1/10;
+    } while(n2>0)
+    {
+        r =n2%10;
+        armstrong =armstrong +pow(r,d);
+        n2 =n2/10;
+    } if (armstrong ==n){
+        printf("%d is armstrong",armstrong);
+    } else {
+        printf("NOT a armstrong number");
+    } 
     return 0;
 }

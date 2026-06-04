87428a1df9f6e79f4f783ceac933e3f2259e0a2e
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-04 21:08:26 +0530

    arnstrong no. in a range

--- Day4/q4.c
+++ Day4/q4.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
-
-int main() {
-    int start, end, num, temp, rem, sum;
-
-    printf("Enter starting number: ");
-    scanf("%d", &start);
-
-    printf("Enter ending number: ");
-    scanf("%d", &end);
-
-    for(num = start; num <= end; num++) {
-
-        temp = num;
-        sum = 0;
-
-        while(temp > 0) {
-            rem = temp % 10;
-            sum = sum + (rem * rem * rem);
-            temp = temp / 10;
-        }
-
-        if(sum == num)
-            printf("%d ", num);
-    }
-
-    return 0;
-}
+#include<stdio.h>
+#include<math.h>
+int armstrong(int number){ 
+ int originalnumber = number, sum=0,x=0,y=0; 
+while(number>0){ 
+ x++; 
+ number = number/10;} 
+ for(int i=1; i<=x; i++){ 
+ y= originalnumber%10; 
+ if(x==3){ 
+ sum = sum + y*y*y; 
+ } else{ sum = sum + pow(y,x);} 
+ originalnumber = originalnumber/10;} 
+return sum; 
+} 
+int main(){ 
+int number1, number2, x; 
+printf("enter starting number = "); 
+scanf("%d", &number1); 
+printf("enter ending number = "); 
+scanf("%d", &number2); 
+for(int i=number1;i<=number2;i++){ 
+ x =armstrong(i); 
+if(x==i){ 
+ printf("%d ",i); 
+ } 
+} 
+return 0;}

8f3f242f7b293561657f0921916fe570144c2bc6
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-05 20:31:05 +0530

    PERFECT NUMBER

---
+++ Day5/q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    int n,i,sum =0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<n;i++){
+        if(n%i==0){
+            sum = sum +i;
+        }
+    } printf("%d is Perfect number",n);
+    return 0;
+}

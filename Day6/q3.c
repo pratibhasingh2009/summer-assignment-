7e367b7bffe4f38a02054524c9407877cf2c8d58
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-06 20:15:56 +0530

    count number of bits

---
+++ Day6/q3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,i,count=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(n>0){
+        if(n%2 ==1){
+            count++;
+        }
+         n =n/2;
+    } printf("Number of sum of bits is %d",count);
+    return 0;
+}

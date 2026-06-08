c47b6df8706d5dbb9f360a167b0cd876610870b7
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-08 20:56:09 +0530

    print half pyramid pattern

--- Day8/q1.c
+++ Day8/q1.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-int main(){
-    int i,n,j;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        for(j=1;j<=i;j++){
-            printf("%d",j);
-        }
-         printf("\n");
-    } 
-    return 0;
+int main(){ 
+int n,i,j; 
+printf("Enter number = "); 
+scanf("%d", &n); 
+for(int i=1;i<=n;i++){ 
+       for(int j=1 ;j<=i;j++){ 
+          printf("*");
+} 
+    printf("\n"); 
+} 
+return 0;
 }

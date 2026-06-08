e4c0ac3a575cd6e74a2d848bd1159be3b31dd23b
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-08 22:26:24 +0530

    print character triangle

--- Day8/q3.c
+++ Day8/q3.c
@@ -6,7 +6,7 @@ int main(){
     scanf("%d",&n);
     for(i=1;i<=n;i++){
         for(j=65;j<=i+64;j++){
-            printf("%c",j);
+            printf("%c ",j);
         }
          printf("\n");
     } 

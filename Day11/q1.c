98f997e83883a1ba62c75ab3245720f1c47268d7
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-14 16:31:22 +0530

    sum by function

--- Day11/q1.c
+++ Day11/q1.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 void sum(int a,int b){
-    int sum =a +b;
+    int add =a+b;
+    printf("%d is sum of %d and %d",add,a,b);
 } 
 int main(){
-    int n1,n2 ;
+    int n1,n2,add ;
     void sum(int a,int b);
     printf("Enter two numbers:");
     scanf("%d %d",&n1,&n2);
-    printf("%d is sum of %d and %d.",sum,n1,n2);
+    sum(n1,n2);
     return 0;
 }

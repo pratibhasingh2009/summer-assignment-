3b3848ea2a81ada88f1324a3f3d6fbc75bdd9292
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-15 19:19:20 +0530

    print array elements

--- Day13/q1.c
+++ Day13/q1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int n,i,arr[n];
+    int n,i,arr[100];
     printf("Enter a number:");
     scanf("%d",&n);
     printf("Enter elements of array:");
@@ -9,6 +9,6 @@ int main(){
     }
     printf("Array elements are :\n");
      for(i=0;i<n;i++){
-        printf("%d",arr[i]);
+        printf("%d\n",arr[i]);
     } return 0;
 }

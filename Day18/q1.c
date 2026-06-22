f426f5fe28c68ca080dbf53852b7c2f9a55dc1a4
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-22 18:13:45 +0530

    bubble sort

---
+++ Day18/q1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int i,n,j,temp;
+    printf("Enter a element:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter elements of array:");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n-1;i++){
+       for(j=0;j<n-1;j++) {
+        if(arr[j]>arr[j+1]){
+            temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+       }
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}

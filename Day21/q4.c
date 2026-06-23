e33ce80e9ae34520afc4b8451c417dc1354a38df
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-23 21:06:51 +0530

    convert lowercase to uppercase

--- Day21/q4.c
+++ Day21/q4.c
@@ -4,10 +4,13 @@ int main(){
  int i=0,n;
   printf("Enter a number:");
   scanf("%d",&n);
+
   char str[n+1];
   getchar();
+
   printf("Enter a String:");
   fgets(str,n+1,stdin);
+  
   for(i=0;str[i]!='\0' && str[i]!='\n';i++)
   {
         if(str[i]>='a' && str[i]<='z')

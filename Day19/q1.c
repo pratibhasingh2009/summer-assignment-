d47569e3999b5ff61dfcfe49c091a5e05a72f096
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-23 20:41:12 +0530

    add matrices

--- Day19/q1.c
+++ Day19/q1.c
@@ -1,30 +1,44 @@
 #include<stdio.h>
-int main(){
+int main()
+{
     int m,n,j,i;
     printf("Enter row and column number :");
     scanf("%d%d",&m,&n);
-    int arr[m][n],brr[m][n],crr[n][m];
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            scanf("%d",&arr[i][j]);
+    int a[m][n],b[m][n],c[m][n];
+    printf("Enter element of matrix A:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+} } 
+    printf("\nEnter element of matrix B:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&b[i][j]);
+           }
+        }
+        printf("matrix A:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d ",a[i][j]);
         } 
         printf("\n");
   }
-     
-    
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            scanf("%d",&brr[i][j]);
+  printf("matrix B:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d ",b[i][j]);
         } 
-         printf("\n");
+          printf("\n");
     }
-     printf("Sum of matrixes");
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            crr[i][j]=arr[i][j]+brr[i][j];
-            printf("%d",crr[i][j]);
+ printf("Sum of matrixes\n");
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+            printf("%d ",c[i][j]);
         }
         printf("\n");
     }
-  return 0;
-}
+    return 0;
+} 

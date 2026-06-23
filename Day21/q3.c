7e9050c8a5fdfa4a4c63ae7d2c955a3ff72e8025
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-23 21:02:56 +0530

    count vowels and consonant

--- Day21/q3.c
+++ Day21/q3.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
-    int i=0,n,vow=0,conso=0;
+
+int i=0,n,vow=0,conso=0;
   printf("Enter a number:");
   scanf("%d",&n);
   char str[n+1];
@@ -8,10 +9,11 @@ int main(){
   printf("Enter a String:");
   fgets(str,n+1,stdin);
   while(str[i]!='\0' && str[i]!='\n'){
-   if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
+   if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+   {
     vow++;
    }
-   else if((str[i]>='a'&& str[i]<='z')|| (str[i]>='A'&& str[i]<='Z')){
+     else if((str[i]>='a'&& str[i]<='z')|| (str[i]>='A'&& str[i]<='Z')){
     conso++;
    }
    i++;

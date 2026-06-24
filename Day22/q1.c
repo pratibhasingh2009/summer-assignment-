c63e8fc90e20a1d0b3c033256079a9ccf2fc8772
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-24 23:25:35 +0530

    palindrome string

---
+++ Day22/q1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+
+ int i,n,len=0,flag=1;
+  printf("Enter a number:");
+  scanf("%d",&n);
+
+  char str[n+1];
+  getchar();
+
+  printf("Enter a String:");
+  fgets(str,n+1,stdin);
+  
+  for(i=0;str[i]!='\0' && str[i]!='\n';i++)
+  {
+    len++;
+  }
+for(i=0;i<len/2;i++){
+        if(str[i]!=str[len-1-i])
+        {
+            flag=0;
+            break;
+        }
+  }
+  if(flag==1){
+    printf("Number is Palindrome.");
+  }
+  printf("Number is not palindrome.");
+  return 0;
+}

38f60c2f721f32dda831ab148c95f96155129685
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-25 22:34:41 +0530

    first non repeating character

---
+++ Day23/q1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+
+ int i,n,j,count;
+  printf("Enter a number:");
+  scanf("%d",&n);
+
+  char str[n+1];
+  getchar();
+
+  printf("Enter a String:");
+  fgets(str,n+1,stdin);
+  for(i=0;str[i]!='\0' && str[i]!='\n';i++)
+  {
+      count=0;
+    for(j=0;str[j]!='\0' && str[j]!='\n';j++)
+ {
+    if(str[i]==str[j]){
+        count++;
+    }
+ } 
+  if(count==1){
+    printf("First non repeating character: %c",str[i]);
+    break;
+    }
+  } 
+    return 0;
+} 

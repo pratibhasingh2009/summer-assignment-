e546be02d5c4241422394e3469ed95bc683885c7
pratibhasingh2009 <ritasingh050684@gamil.com>
2026-06-30 18:12:54 +0530

    compress a string

--- Day24/q2.c
+++ Day24/q2.c
@@ -11,11 +11,12 @@ int main(){
   fgets(str,n+1,stdin);
   for(i=0;str[i]!='\0' && str[i]!='\n';i++){
     c=1;
-      while (str[i]==str[i+1]){
+      while (str[i]==str[i+1])
+      {
         c++;
         i++;
       }
       printf("%c%d",str[i],c);
-  }
+   }
   return 0;
 }
